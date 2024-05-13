fe22ff58bff5509daf36c2d2e331aed042cd5828
Cure-X <admin@cure-x.net>
2024-05-13 19:50:37 +0800

    add exec: loop

--- include/ast/statements/loops/condition_loop.hpp
+++ include/ast/statements/loops/condition_loop.hpp
@@ -4,6 +4,8 @@
 #include "ast/statements/statement.hpp"
 #include "ast/statements/statement_block.hpp"
 
+#include "utils/call_method.hpp"
+
 class condition_loop : public statement {
 public:
 	condition_loop(expr* Condition, statement_block* Block, int begin, int end) : statement(begin, end), CONDITION(Condition), BODY(Block) {};
@@ -14,5 +16,9 @@ public:
 		return L"loop (" + this->CONDITION->view() + L") {\n" + block_indent(this->BODY->view()) + L"}\n";
 	}
 
-	void exec(environment env, const runtime& Runtime) {}; //!
+	void exec(environment env, const runtime& Runtime) {
+		while (dynamic_cast<builtin_object_bool*> (call_method(dynamic_cast<semantic_object*> (this->CONDITION->eval(env, Runtime)), L"bool")) -> data) {
+			this->BODY->exec(env, Runtime);
+		}
+	};
 };
